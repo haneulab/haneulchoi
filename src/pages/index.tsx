bebd79174fbc7c4ba65f1acecae97dc66d17f3f4
haneulab <hanchoi@ucdavis.edu>
2023-02-23 15:09:04 -0800

    feat: landing page redesign

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import { PrimaryLayout } from '@haneulchoi/components/layouts'
 import { useTheme } from '@haneulchoi/contexts'
 import { classnames } from '@haneulchoi/functions'
 
+import Image from 'next/image'
+import HaneulChoiAvatar from 'public/haneulchoi_avatar.png'
+
 const getServerSideProps = async (_: GetServerSidePropsContext) => {
     return { props: {} }
 }
@@ -17,71 +20,175 @@ const HomePage: PageWithLayout = () => {
     return (
         <>
             <div className="min-h-screen w-full flex flex-col justify-center relative">
-                {/* <picture className="hidden md:block w-full h-full absolute top-0 z-0">
-                    <Image
-                        src={BGDesktop}
-                        alt=""
-                        className="opacity-10 w-full"
-                    />
-                </picture>
-                <picture className="block md:hidden w-full h-full absolute top-0 z-0">
-                    <Image
-                        src={BGMobile}
-                        alt=""
-                        className="opacity-10 w-full"
-                    />
-                </picture> */}
-                <section className="h-[60vh] w-full flex flex-col justify-center">
-                    <section className="w-full max-w-cutoff mx-auto px-4 py-8 grid grid-cols-1 lg:grid-cols-8 xl:grid-cols-12 gap-8">
-                        <article className="flex flex-col items-center lg:items-start lg:col-span-6 xl:col-span-7">
-                            <h2 className="font-lato font-bold text-xl lg:text-3xl mb-2 lg:mb-3 italic">
-                                Hello, I&apos;m Haneul
-                            </h2>
-                            <h3 className="font-bold font-poppins text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl mb-4 lg:mb-5">
-                                I Make Websites.
-                            </h3>
-                            <p className="font-nunito text-center lg:text-left font-light text-lg leading-7 lg:text-xl lg:leading-8 2xl:text-2xl 2xl:leading-9">
-                                I am a passionate software developer who loves
-                                to solve seemlingly impossible problems.
-                            </p>
-                        </article>
-                        <ul className="flex lg:flex-col justify-center items-center gap-x-4 lg:gap-x-0 lg:items-end lg:col-span-2 xl:col-span-5 gap-y-8">
-                            <button className="p-1 w-max rounded-md transition-smooth hover:bg-neutral-900 hover:text-white transform lg:hover:scale-110">
-                                <span className="rounded-md w-[90%] h-[90%] mx-auto flex items-center justify-center bg-transparent transition-smooth">
-                                    <SiGithub className="text-4xl lg:text-5xl xl:text-6xl" />
-                                </span>
+                <section className="px-12 lg:px-8 py-24 lg:py-28 w-full max-w-cutoff mx-auto grid grid-cols-1 gap-9 lg:grid-cols-12">
+                    <article className="flex flex-col items-center lg:items-start lg:col-span-9">
+                        <picture className="block w-full max-w-[500px]">
+                            <Image
+                                src={HaneulChoiAvatar}
+                                alt="Haneul Choi Avatar Image"
+                            />
+                        </picture>
+                        <code className="text-center lg:text-start lg:w-full w-max mx-auto font-nunito font-medium text-xl md:text-2xl lg:text-3xl block mb-2">
+                            Hi, I&apos;m Haneul.
+                        </code>
+                        <h2 className="font-medium font-poppins text-center text-2xl lg:text-3xl">
+                            I&apos;m a happy software developer.
+                        </h2>
+                    </article>
+                    <ul
+                        className={classnames(
+                            'flex items-center gap-x-4 justify-center lg:flex-col lg:gap-x-0 lg:gap-y-4 xl:gap-y-6 col-span-1 lg:col-span-3',
+                            theme === 'dark' ? '' : 'text-white'
+                        )}
+                    >
+                        <button className="bg-gradient-to-br from-cyan-500 via-sky-500 to-blue-500 p-1.5 rounded-md flex justify-center transition-smooth transform hover:rotate-[25deg] hover:scale-90 items-center">
+                            <span className="mx-auto w-[90%] h-[90%] flex flex-col items-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl transform transition-smooth hover:scale-[180%] hover:w-full hover:h-full hover:bg-white/5 backdrop-blur-md rounded-full">
+                                <SiGithub />
+                            </span>
+                        </button>
+                        <button className="bg-gradient-to-br from-cyan-500 via-sky-500 to-blue-500 p-1.5 rounded-md flex justify-center transition-smooth transform hover:rotate-[25deg] hover:scale-90 items-center">
+                            <span className="mx-auto w-[90%] h-[90%] flex flex-col items-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl transform transition-smooth hover:scale-[180%] hover:w-full hover:h-full hover:bg-white/5 backdrop-blur-md rounded-full">
+                                <SiLinkedin />
+                            </span>
+                        </button>
+                        <button className="bg-gradient-to-br from-cyan-500 via-sky-500 to-blue-500 p-1.5 rounded-md flex justify-center transition-smooth transform hover:rotate-[25deg] hover:scale-90 items-center">
+                            <span className="mx-auto w-[90%] h-[90%] flex flex-col items-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl transform transition-smooth hover:scale-[180%] hover:w-full hover:h-full hover:bg-white/5 backdrop-blur-md rounded-full">
+                                <SiGmail />
+                            </span>
+                        </button>
+                    </ul>
+                </section>
+            </div>
+            <div className="min-h-screen">
+                <section className="px-12 lg:px-8 py-12 lg:py-16 w-full h-full max-w-cutoff mx-auto grid grid-cols-1 gap-y-0 lg:grid-cols-12 gap-x-0">
+                    <article
+                        className={classnames(
+                            'col-span-2 lg:col-span-3 w-full h-full flex flex-col gap-y-4 text-center p-8',
+                            theme === 'dark' ? 'bg-zinc-800' : 'bg-slate-200'
+                        )}
+                    >
+                        <h3 className="font-bold text-2xl mb-4">
+                            Software Projects
+                        </h3>
+                        <ul className="flex flex-col items-center gap-y-4">
+                            <button className="font-nunito font-medium text-lg text-slate-400 transition-smooth hover:text-slate-500">
+                                Web Applications
                             </button>
-                            <button className="p-1 w-max rounded-md transition-smooth hover:bg-neutral-900 hover:text-white transform lg:hover:scale-110">
-                                <span className="rounded-md w-[90%] h-[90%] mx-auto flex items-center justify-center bg-transparent transition-smooth">
-                                    <SiLinkedin className="text-4xl lg:text-5xl xl:text-6xl" />
-                                </span>
+                            <button className="font-nunito font-medium text-lg text-slate-400 transition-smooth hover:text-slate-500">
+                                Software Programs
                             </button>
-                            <button className="p-1 w-max rounded-md transition-smooth hover:bg-neutral-900 hover:text-white transform lg:hover:scale-110">
-                                <span className="rounded-md w-[90%] h-[90%] mx-auto flex items-center justify-center bg-transparent transition-smooth">
-                                    <SiGmail className="text-4xl lg:text-5xl xl:text-6xl" />
-                                </span>
+                            <button className="font-nunito font-medium text-lg text-slate-400 transition-smooth hover:text-slate-500">
+                                GUI Games
                             </button>
                         </ul>
-                    </section>
-                </section>
-                <section
-                    className={classnames(
-                        'min-h-[40vh] w-full border-t',
-                        theme === 'dark'
-                            ? 'border-slate-600'
-                            : 'border-slate-200'
-                    )}
-                >
-                    <section className="px-8 lg:px-4 py-8 flex flex-col max-w-cutoff mx-auto w-full">
-                        <article>
-                            <h3 className="font-bold font-lato text-2xl md:text3-xl lg:text-4xl mb-2">
-                                Featured Projects
-                            </h3>
-                            <p className="text-red-400">
-                                Currently preparing this section.
-                            </p>
-                        </article>
-                    </section>
+                    </article>
+                    <article
+                        className={classnames(
+                            'col-span-4 lg:col-span-9 pt-8 lg:pt-0 lg:pl-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3',
+                            theme === 'dark' ? 'bg-neutral-900' : 'bg-white'
+                        )}
+                    >
+                        <section
+                            className={classnames(
+                                'lg:border lg:drop-shadow lg:p-6  flex flex-col justify-between gap-y-2 transition-smooth transform hover:scale-[102.5%] cursor-pointer',
+                                theme === 'dark'
+                                    ? 'border-slate-400/30 hover:border-slate-400/50'
+                                    : 'hover:border-neutral-900/75 lg:border-neutral-900/25 shadow-md shadow-transparent hover:shadow-slate-200/50 '
+                            )}
+                        >
+                            <div className="mb-2">
+                                <h4 className="font-medium text-xl font-poppins mb-2">
+                                    Soboro
+                                </h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nemo officia ex et
+                                    accusantium facilis aut.
+                                </p>
+                            </div>
+                            <div>
+                                <button className="px-4 py-3 inline-flex justify-center items-center gap-x-2 w-full bg-zinc-500 text-neutral-50 font-lato font-bold text-sm transition-smooth hover:bg-neutral-500/50">
+                                    <SiGithub />
+                                    <span>Repository</span>
+                                </button>
+                            </div>
+                        </section>
+                        <section
+                            className={classnames(
+                                'lg:border lg:drop-shadow lg:p-6  flex flex-col justify-between gap-y-2 transition-smooth transform hover:scale-[102.5%] cursor-pointer',
+                                theme === 'dark'
+                                    ? 'border-slate-400/30 hover:border-slate-400/50'
+                                    : 'hover:border-neutral-900/75 lg:border-neutral-900/25 shadow-md shadow-transparent hover:shadow-slate-200/50 '
+                            )}
+                        >
+                            <div className="mb-2">
+                                <h4 className="font-medium text-xl font-poppins mb-2">
+                                    Soboro
+                                </h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nemo officia ex et
+                                    accusantium facilis aut.
+                                </p>
+                            </div>
+                            <div>
+                                <button className="px-4 py-3 inline-flex justify-center items-center gap-x-2 w-full bg-zinc-500 text-neutral-50 font-lato font-bold text-sm transition-smooth hover:bg-neutral-500/50">
+                                    <SiGithub />
+                                    <span>Repository</span>
+                                </button>
+                            </div>
+                        </section>
+                        <section
+                            className={classnames(
+                                'lg:border lg:drop-shadow lg:p-6  flex flex-col justify-between gap-y-2 transition-smooth transform hover:scale-[102.5%] cursor-pointer',
+                                theme === 'dark'
+                                    ? 'border-slate-400/30 hover:border-slate-400/50'
+                                    : 'hover:border-neutral-900/75 lg:border-neutral-900/25 shadow-md shadow-transparent hover:shadow-slate-200/50 '
+                            )}
+                        >
+                            <div className="mb-2">
+                                <h4 className="font-medium text-xl font-poppins mb-2">
+                                    Soboro
+                                </h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nemo officia ex et
+                                    accusantium facilis aut.
+                                </p>
+                            </div>
+                            <div>
+                                <button className="px-4 py-3 inline-flex justify-center items-center gap-x-2 w-full bg-zinc-500 text-neutral-50 font-lato font-bold text-sm transition-smooth hover:bg-neutral-500/50">
+                                    <SiGithub />
+                                    <span>Repository</span>
+                                </button>
+                            </div>
+                        </section>
+                        <section
+                            className={classnames(
+                                'lg:border lg:drop-shadow lg:p-6  flex flex-col justify-between gap-y-2 transition-smooth transform hover:scale-[102.5%] cursor-pointer',
+                                theme === 'dark'
+                                    ? 'border-slate-400/30 hover:border-slate-400/50'
+                                    : 'hover:border-neutral-900/75 lg:border-neutral-900/25 shadow-md shadow-transparent hover:shadow-slate-200/50 '
+                            )}
+                        >
+                            <div className="mb-2">
+                                <h4 className="font-medium text-xl font-poppins mb-2">
+                                    Soboro
+                                </h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nemo officia ex et
+                                    accusantium facilis aut.
+                                </p>
+                            </div>
+                            <div>
+                                <button className="px-4 py-3 inline-flex justify-center items-center gap-x-2 w-full bg-zinc-500 text-neutral-50 font-lato font-bold text-sm transition-smooth hover:bg-neutral-500/50">
+                                    <SiGithub />
+                                    <span>Repository</span>
+                                </button>
+                            </div>
+                        </section>
+                    </article>
                 </section>
             </div>
         </>
