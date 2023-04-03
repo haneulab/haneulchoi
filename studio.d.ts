8c9f1178dc6c25a9bfe0d596ffb4cd4dad9f5ff5
haneulab <hanchoi@ucdavis.edu>
2023-04-03 12:41:25 -0700

    setup: studio basic type declarations

---
+++ studio.d.ts
@@ -0,0 +1,27 @@
+import type {
+    GetServerSideProps,
+    GetServerSidePropsContext,
+    NextPage,
+} from 'next'
+import type { AppProps } from 'next/app'
+
+declare module 'studio' {
+    type StudioPage<P = unknown, IP = P> = NextPage<P, IP> & {
+        layout?: React.ComponentType
+        getLayout?: (_page: React.ReactElement) => React.ReactNode
+    }
+    type StudioProps<P = Record<string, unknown>> = {
+        Component: StudioPage
+    } & AppProps<P>
+    type StudioSSRProps = GetServerSideProps
+    type StudioSSRContext = GetServerSidePropsContext
+
+    type ThemeOptions = 'dark' | 'light'
+    type ThemeStore = {
+        theme: ThemeOptions
+        onChange: (_theme?: ThemeOptions) => void
+    }
+    type ThemeStoreProvider = (_props: {
+        children: React.ReactNode
+    }) => React.ReactNode
+}
